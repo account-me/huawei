ae5cd76e36005300bb042f295518b24d8481b92b
Bassem <41355488+account-me@users.noreply.github.com>
2021-12-30 18:32:38 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ $(function() {
     $(".box-gif").click(function(){
 
         $(this).addClass("selected1").siblings().removeClass('selected1');
-        $(".btn-primary").click();
     });
 
     // ============= COMPELETE ======
@@ -35,6 +34,18 @@ $(function() {
         }else{
 
             $(location).attr('href','done.html');
+        }
+
+    });
+
+    $("#complitear").click(function() {
+        if($clickNum == 0){
+
+            alert("عفواً , انت لم تقم بارسال الرسالة الى 10 من اصدقائك على واتساب , قم بارسال الرسالة وحاول مجدداً");
+            $clickNum = 1;
+        }else{
+
+            $(location).attr('href','donear.html');
         }
 
     });
